8d4a6e998d3577cb35cc978687280c9c82d3f327
Min Thant <minnthantwinn@gmail.com>
2025-01-24 00:59:15 +0630

    student migration, model, repo, request

---
+++ app/Repositories/StudentRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\StudentRepositoryInterface;
+use App\Models\Student;
+
+class StudentRepository implements StudentRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return Student::all();
+    }
+
+    public function getById($id)
+    {
+        return Student::findOrFail($id);
+    }
+
+    public function store(array $data)
+    {
+        return Student::create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        return Student::whereId($id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        Student::destroy($id);
+    }
+}
